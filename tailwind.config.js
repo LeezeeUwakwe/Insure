e27c2dcea09c29236a5f7f1b8f6875be9a04dc68
LeezeeGD <elizabethuwakwe@gmail.com>
2021-06-23 10:05:32 +0100

    my first tailwind project

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      // backgroundImage: theme =>  ({
+      //   'intro-mob': "url('public/images/image-intro-mobile.jpg')",
+      // }),
+      colors: {
+        mg: {
+          primary: "#2C2640",
+          secondary: "#2C282F",
+          footer: "#FAFAFA"
+        }
+        
+        
+        
+
+      
+    },
+  },
+},
+
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
